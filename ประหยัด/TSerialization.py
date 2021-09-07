84463d71751cba15bc1b0cf0ad0de06292485d57
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-07 13:01:16 +0700

    Add files via upload

--- ประหยัด/TSerialization.py
+++ ประหยัด/TSerialization.py
@@ -17,20 +17,22 @@
 # under the License.
 #
 
-from os import path
-from SCons.Builder import Builder
-from six.moves import map
+from .protocol import TBinaryProtocol
+from .transport import TTransport
 
 
-def scons_env(env, add=''):
-    opath = path.dirname(path.abspath('$TARGET'))
-    lstr = 'thrift --gen cpp -o ' + opath + ' ' + add + ' $SOURCE'
-    cppbuild = Builder(action=lstr)
-    env.Append(BUILDERS={'ThriftCpp': cppbuild})
+def serialize(thrift_object,
+              protocol_factory=TBinaryProtocol.TBinaryProtocolFactory()):
+    transport = TTransport.TMemoryBuffer()
+    protocol = protocol_factory.getProtocol(transport)
+    thrift_object.write(protocol)
+    return transport.getvalue()
 
 
-def gen_cpp(env, dir, file):
-    scons_env(env)
-    suffixes = ['_types.h', '_types.cpp']
-    targets = map(lambda s: 'gen-cpp/' + file + s, suffixes)
-    return env.ThriftCpp(targets, dir + file + '.thrift')
+def deserialize(base,
+                buf,
+                protocol_factory=TBinaryProtocol.TBinaryProtocolFactory()):
+    transport = TTransport.TMemoryBuffer(buf)
+    protocol = protocol_factory.getProtocol(transport)
+    base.read(protocol)
+    return base
