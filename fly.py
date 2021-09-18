d487d98a9357d2628885ca07d8c7b838d7bfb72b
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-19 02:47:17 +0700

    Update fly.py

--- fly.py
+++ fly.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from time import sleep
 from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib.request, urllib.parse, urllib.error, urllib.parse,antolib,subprocess,unicodedata,GACSender
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib.request, urllib.parse, urllib.error, urllib.parse,antolib,subprocess,unicodedata,
 from gtts import gTTS
 from googletrans import Translator
 #==============================================================================#
